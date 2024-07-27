c8315220b3764afee9ef866d607b174a27c83359
Jan Vollmer <jan@vllmr.dev>
2024-07-27 16:15:50 +0200

    configure plasma as default session and start sddm using wayland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -188,14 +188,21 @@
 
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland = { enable = true; };
+    };
+    defaultSession = "plasma";
+  };
   services.desktopManager.plasma6.enable = true;
 
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "de";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Configure console keymap
