76acd53f87eaac479e8d09e8c50f5a652c7aab16
Jan Vollmer <jan@vllmr.dev>
2024-07-27 19:42:34 +0200

    put nvidia config into extra file; dont use nixos-unstable

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,9 +18,10 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
-
+    ./nvidia.nix
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+
   ];
 
   nixpkgs = {
@@ -62,7 +63,7 @@
         nix-path = config.nix.nixPath;
       };
       # Opinionated: disable channels
-      channel.enable = false;
+      channel.enable = true;
 
       # Opinionated: make flake registry and nix path match flake inputs
       registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
@@ -74,9 +75,6 @@
 
 
   boot = {
-    kernelParams = [
-      "module_blacklist=i915" # intel video driver
-    ];
     loader = {
 
 
@@ -118,59 +116,12 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = lib.mkDefault true;
-    driSupport32Bit = lib.mkDefault true;
-    #---------------------------------------------------------------------
-    # Install additional packages that improve graphics performance and compatibility.
-    #---------------------------------------------------------------------
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      libvdpau-va-gl
-      nvidia-vaapi-driver
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      vulkan-validation-layers
-    ];
-  };
+
 
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia = {
-
-
-    modesetting.enable = true;
-    nvidiaPersistenced = true;
-
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-
-
-  };
 
   # Set environment variables related to NVIDIA graphics
   environment.variables = {
