032fa949c6384dd98653ae250f47a650d1132483
Jan Vollmer <jan@vllmr.dev>
2024-07-27 20:05:16 +0200

    only apply nvidia config settings if an nvidia card is present

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -1,60 +1,67 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let
+  hasNvidia = config.hardware.nvidia != null;
+in
+{
+  config = lib.mkIf hasNvidia {
+    boot = {
+      kernelParams = [
+        "module_blacklist=i915" # blacklist intel video driver
+      ];
+    };
 
-  boot = {
-    kernelParams = [
-      "module_blacklist=i915" # blacklist intel video driver
-    ];
+    # Enable OpenGL
+    hardware.opengl = {
+      enable = true;
+      driSupport = lib.mkDefault true;
+      driSupport32Bit = lib.mkDefault true;
+      #---------------------------------------------------------------------
+      # Install additional packages that improve graphics performance and compatibility.
+      #---------------------------------------------------------------------
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        libvdpau-va-gl
+        nvidia-vaapi-driver
+        vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        vaapiVdpau
+        vulkan-validation-layers
+      ];
+    };
+
+    hardware.nvidia = {
+
+
+      modesetting.enable = true;
+      nvidiaPersistenced = true;
+
+      powerManagement.enable = true;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+
+
+    };
+
   };
 
-  # Enable OpenGL
-  hardware.opengl = {
-    enable = true;
-    driSupport = lib.mkDefault true;
-    driSupport32Bit = lib.mkDefault true;
-    #---------------------------------------------------------------------
-    # Install additional packages that improve graphics performance and compatibility.
-    #---------------------------------------------------------------------
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      libvdpau-va-gl
-      nvidia-vaapi-driver
-      vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      vaapiVdpau
-      vulkan-validation-layers
-    ];
-  };
-
-  hardware.nvidia = {
-
-
-    modesetting.enable = true;
-    nvidiaPersistenced = true;
-
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of
-    # supported GPUs is at:
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-    # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-
-
-  };
 
 
 
