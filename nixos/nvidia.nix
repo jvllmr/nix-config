e5f789e9d79d641410a3f1590b8ef2415d74621e
Jan Vollmer <jan@vllmr.dev>
2024-07-28 14:23:25 +0200

    switch to production nvidia driver

--- nixos/nvidia.nix
+++ nixos/nvidia.nix
@@ -39,7 +39,7 @@ in
 
 
       modesetting.enable = true;
-      nvidiaPersistenced = true;
+      nvidiaPersistenced = false;
 
       powerManagement.enable = false;
 
@@ -61,7 +61,7 @@ in
       nvidiaSettings = true;
 
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
 
 
 
