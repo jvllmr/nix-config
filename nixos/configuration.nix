c7bcafb307f6fbe57edbc02a237d7f305e35ac7a
Jan Vollmer <jan@vllmr.dev>
2024-09-03 19:27:21 +0200

    don't use wayland with sddm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -133,7 +133,7 @@
   services.displayManager = {
     sddm = {
       enable = true;
-      wayland = { enable = true; };
+      wayland = { enable = false; };
     };
     defaultSession = "plasma";
   };
